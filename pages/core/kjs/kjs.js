c2059cb319db8665c75223906787b7d0f4841bf0
wupengju <3145679267@qq.com>
2016-11-02 14:55:17 +0800

    wupengju-kjs-交互完成-xs-交互完成-交互设计文档

--- pages/core/kjs/kjs.js
+++ pages/core/kjs/kjs.js
@@ -18,18 +18,29 @@ function sendRequest(requestData){
   function doSuccess(data) {
 
     that.setData({
-      'testData': data
+      'testData': data,
+      'errObj.errorDisplay': true
     });
   }
 
   // 对失败进行处理
   function doFail(err) {
 
+    var status = typeof err === 'string' ? '403' : err.status,
+        errorMessage = typeof err === 'string' ? err : err.message;
+
     that.setData({
       'errObj.errorDisplay': false,
-      'errObj.status': err.status,
-      'errObj.errorMessage': err.message
+      'errObj.status': status,
+      'errObj.errorMessage': errorMessage
     });
+  }
+
+  console.log(requestData);
+
+  if (requestData.classNo === '') {
+    doFail('查询的时间段不能为空!');
+    return false;
   }
 
   // 发送请求
@@ -44,6 +55,16 @@ function sendRequest(requestData){
     },
     fail: function(err) {
       doFail(err);
+    },
+    complete: function () {
+
+      // 将请求条件状态设置为上一次请求的状态
+      that.setData({
+        'requestData.weekNo': requestData.weekNo,
+        'requestData.weekDay': requestData.weekDay,
+        'requestData.buildingNo': requestData.buildingNo,
+        'requestData.classNo': requestData.classNo
+      });
     }
   });
 }
@@ -71,7 +92,14 @@ Page({
     conditionSigns: { // 保存即时被点击选中的条件的显示的标识对象
       weekSign: 1,
       daySign: 1,
-      classTimeSign: '1@2',
+      classTimeSign: {
+        time0: true,
+        time1: false,
+        time2: false,
+        time3: false,
+        time4: false,
+        time5: false
+      },
       buildingSign: 2
     },
     testData: null
@@ -110,11 +138,83 @@ Page({
   // classTime
   chooseClaasTime: function (e) {
     
-    var index = e.target.dataset.classno;
+    var arrIndex = ['1@2', '3@4', '5@6', '7@8', '9@10', '11@12'],
+        index = e.target.dataset.classno,
+        classNo = this.data.requestData.classNo,
+        arrIndexNo = arrIndex.indexOf(index),
+        strSetData = '',
+        curClassTimeSign = this.data.conditionSigns.classTimeSign[ 'time' + arrIndexNo ];
+    
+    classNo = classNo.replace(/%40/g, '@');
+    classNo += '@' + index;
 
+    // 按照字符串在classNo中出现的次数来决定删除或留下   奇——留下  偶——删除
+    function delSaceStr(str) {
+
+      var arrStr = str.split('@'),
+          sameObj = {
+            time12: 0,
+            time34: 0,
+            time56: 0,
+            time78: 0,
+            time910: 0,
+            time1112: 0
+          },
+          strObj = {
+            time12: '1@2',
+            time34: '3@4',
+            time56: '5@6',
+            time78: '7@8',
+            time910: '9@10',
+            time1112: '11@12'
+          },
+          key = '',
+          retStr = '';
+
+      arrStr.forEach(function(item, index){
+
+        switch(item) {
+          case '1':
+            sameObj.time12++;
+            break;
+          case '3':
+            sameObj.time34++;
+            break;
+          case '5':
+            sameObj.time56++;
+            break;
+          case '7':
+            sameObj.time78++;
+            break;
+          case '9':
+            sameObj.time910++;
+            break;
+          case '11':
+            sameObj.time1112++;
+            break;
+        };
+      });
+
+      for(key in sameObj) {
+
+        if (sameObj.hasOwnProperty(key)) {
+
+          if (sameObj[ key ] % 2 !== 0) {
+            retStr += retStr === '' ? strObj[ key ] : '@' + strObj[ key ];
+          }
+        }
+      }
+
+      return retStr;
+    }
+
+    classNo = delSaceStr(classNo);
+    // console.log(classNo);
+    strSetData = '{"conditionSigns.classTimeSign.time' + arrIndexNo + '":' + !curClassTimeSign + '}';
+
+    this.setData(JSON.parse(strSetData));
     this.setData({
-      'requestData.classNo': index,
-      'conditionSigns.classTimeSign': index
+      'requestData.classNo': classNo // classNo中含有'@'特殊字符  作为属性字符串在JSON字符串中不能转换为JSON对象
     });
   },
 
@@ -158,6 +258,7 @@ Page({
 
   onReady: function(){
 
+    // 初始默认显示
     sendRequest.apply(this, [ this.data.requestData ]);
   }
 });
