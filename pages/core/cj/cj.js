1ef245c4e0d1b86652565e994f45706d98ab7e22
Misic <m18883993593@163.com>
2016-11-11 21:12:27 +0800

    ks&cj with requests

--- pages/core/cj/cj.js
+++ pages/core/cj/cj.js
@@ -3,9 +3,68 @@
 var app = getApp();
 Page({
   data: {
-    
+    cjInfo : [
+
+    ],
+    xqNum : {
+      grade: '',
+      semester: ''
+    },
+    xqName : {
+      grade: '',
+      semester: ''
+    }
   },
-  onReady: function(){
+  onLoad: function(){
+    var _this = this;
+    wx.request({
+      url: "https://we.cqu.pt/api/get_kscj.php",
+      data: {
+        xh: "2014211418",
+        sfzh: "204875"
+      },
+      success: function(res) {
+        console.log(res);
+
+        var _data = res.data.data;
+
+        var term = _data[0].term;
+        var xh = _data[0].xh;
+        var year = term.slice(0,4);
+        var semester = term.slice(4);
+        var yearIn = xh.slice(0,4);
+        var xqNum_grade = year + '-' + (+year+1);
+        var xqNum_semester = semester;
+        var xqName_grade = changeNum(year - yearIn + 1);
+        var xqName_semester = (semester == 1) ? '上' : '下';
+        var xqNum = {
+          grade: xqNum_grade,
+          semester: xqNum_semester
+        }
+        var xqName = {
+          grade: xqName_grade,
+          semester: xqName_semester
+        }
+        
+        _this.setData({
+          cjInfo: _data,
+          xqNum: xqNum,
+          xqName: xqName
+        });
+
+      }
+    });
+
+    function changeNum(num){  
+      var china = new Array('零','一','二','三','四','五','六','七','八','九');  
+      var arr = new Array();  
+      var n = ''.split.call(num,''); 
+      for(var i = 0; i < n.length; i++){  
+        arr[i] = china[n[i]];  
+      }  
+      return arr.join("")  
+    }  
+
 
   }
 });
--- pages/core/ks/ks.js
+++ pages/core/ks/ks.js
@@ -3,17 +3,9 @@
 var app = getApp();
 Page({
   data: {
-    ks: {
-        ksName: '2016-2017年度上学期期末考试',
-        stuId: '2014210172',
-        stuName: '杨奇奇'
-    },
+    ks: {},
     first: 1,
-    class: [
-        { id:'0', ksName: '通信原理A', ksDate: '2017-01-06', ksTime: '9:30-11.30', ksPlace: '三教3106', ksSeat: '28' },
-        { id:'1', ksName: '通信原理A', ksDate: '2017-01-06', ksTime: '9:30-11.30', ksPlace: '三教3106', ksSeat: '28' },
-        { id:'2', ksName: '通信原理A', ksDate: '2017-01-06', ksTime: '9:30-11.30', ksPlace: '三教3106', ksSeat: '28' }
-    ]
+    class: []
   },
   togglePage: function (e) {
     var id = e.currentTarget.id, data = {};
@@ -29,7 +21,57 @@ Page({
     data.show[id] = !this.data.show[id];
     this.setData(data);
   },
-  onReady: function(){
+  onLoad: function(){
+    var _this = this;
+    wx.request({
+      url: "http://we.cqupt.edu.cn/api/get_ks.php",
+      data: {
+        xh: "2014211418",
+        sfzh: "204875"
+      },
+      success: function(res) {
+        console.log(res);
 
+        if (res.data.status == 200){
+
+          var _data = res.data.data;
+        
+          var _class = [],temp;
+          var ks = {
+            ksName: '2016-2017年度上学期期末考试',
+            stuId: _data[0].xh,
+            stuName: _data[0].name
+          }
+
+          for(var i = 0; i < _data.length; i++){
+            (function(i){
+              temp = {
+                id: i,
+                ksName: _data[i].course_name,
+                ksDate: _data[i].date,
+                ksTime: _data[i].time,
+                ksPlace: _data[i].place,
+                ksSeat: _data[i].number
+              }
+              _class.push(temp);
+            })(i);
+          }
+
+          console.log(_class);
+
+          _this.setData({
+            class: _class,
+            ks: ks
+          });
+
+        } else {
+          wx.showToast({
+            title: res.data.message,
+            duration: 4000
+          })
+        }
+
+      }
+    });
   }
 });
