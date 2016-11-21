197ec649b8053ed63351c09bcd58774ce859e123
Cong Min <i@congm.in>
2016-11-21 21:57:22 +0800

    完善更多页面

--- pages/more/more.js
+++ pages/more/more.js
@@ -14,8 +14,14 @@ Page({
   },
   getData: function(){
     var _this = this;
+    var days = ['一','二','三','四','五','六','日'];
     _this.setData({
       'user': app._user,
+      'time': {
+        'term': app._time.term,
+        'week': app._time.week,
+        'day': days[app._time.day]
+      },
       'is_bind': !!app._user.is_bind
     });
     wx.stopPullDownRefresh();
