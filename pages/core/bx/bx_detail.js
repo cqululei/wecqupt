bfa03f51f33352c40e95fd2172c13bba3b8993f2
Cong Min <i@congm.in>
2016-11-25 21:15:11 +0800

    报修完善

--- pages/core/bx/bx_detail.js
+++ pages/core/bx/bx_detail.js
@@ -1,135 +1,118 @@
 //bx_detail.js
 //获取应用实例
 var app = getApp();
-
-function sendRequest (app, urlFooter, data, method){
-
-  var url = app._server + urlFooter,
-      that = this;
-
-  console.log(url, data, method);
-
-    // 对成功进行处理
-  function doSuccess(data) {
-    
-    that.data.detail.push(data);//存入数组
-    
-
-    var dataLen = data.length;
-    that.data.lastData = data[dataLen - 1];
-
-  }
-
-  // 对失败进行处理
-  function doFail(err) {
-    console.log(err);
-    
-  }
-
-    // 发送请求
-  wx.request({
-    url: url, 
-    data: data,
-    method: method,
-    header: {
-      'Content-Type': 'application/json'
-    },
-    success: function(res) {
-      console.log(res.data.data)
-      doSuccess(res.data.data);
-    },
-    fail: function(err) {
-      doFail(err);
-    },
-    complete: function () {
-
-      console.log(that.data);
-      
-    }
-  });
-};
-
-
-
-
 Page({
   data: {
-    detail:[{
-           wx_djh:"登记号报修记录ID",
-           wx_bt:"标题",
-           wx_bxlxh:"报修类型号",
-           wx_bxlxm:"报修类型名",
-           wx_bxrrzm:"报修人同意识别码",
-           wx_bxr:"报修人",
-           wx_bxsj:"报修时间",
-           wx_bxdh:"报修电话",
-           wx_fwqyh:"服务区域号",
-           wx_bxdd:"报修地点",
-           wx_bxnr:"报修内容",
-           wx_slr:"受理人",
-           wx_shr:"审核人",
-           wx_cxr:"承修人",
-           wx_ysfz:"用时分钟",
-           wx_ysqk:"验收情况",
-           wx_hfr:"回访人",
-           wx_hfmyd:"回访满意度",
-           wx_fwqym:"服务区域名",
-           wx_wxztm:"维修状态名",
-           wx_wxgm:"维修工名",
-           wx_cxbmm:"承修部门名",
-           wx_xysj:"响应时间",
-           wx_wxdh:"维修单据号",
-           wx_bxip:"报修ip",
-           wx_sfkhf:"是否可回访"
-    }],
-    lastDetail:{
-           wx_djh:"登记号报修记录ID",
-           wx_bt:"标题",
-           wx_bxlxh:"报修类型号",
-           wx_bxlxm:"报修类型名",
-           wx_bxrrzm:"报修人同意识别码",
-           wx_bxr:"报修人",
-           wx_bxsj:"报修时间",
-           wx_bxdh:"报修电话",
-           wx_fwqyh:"服务区域号",
-           wx_bxdd:"报修地点",
-           wx_bxnr:"报修内容",
-           wx_slr:"受理人",
-           wx_shr:"审核人",
-           wx_cxr:"承修人",
-           wx_ysfz:"用时分钟",
-           wx_ysqk:"验收情况",
-           wx_hfr:"回访人",
-           wx_hfmyd:"回访满意度",
-           wx_fwqym:"服务区域名",
-           wx_wxztm:"维修状态名",
-           wx_wxgm:"维修工名",
-           wx_cxbmm:"承修部门名",
-           wx_xysj:"响应时间",
-           wx_wxdh:"维修单据号",
-           wx_bxip:"报修ip",
-           wx_sfkhf:"是否可回访"
+    remind: '加载中',
+    detail: {},   //工单详情
+    state: []     //处理详情(申请-审核-受理-派单-完工-驳回.倒序)
+  },
+  //下拉更新
+  onPullDownRefresh: function(){
+    this.getData();
+  },
+  onLoad: function(options){
+    this.setData({
+      bxID: options.id
+    });
+    this.getData();
+  },
+  getData: function () {
+    var _this = this;
+    if(!app._user.xs.ykth || !_this.data.bxID){
+      app.showErrorModal('404');
+      _this.setData({
+        remind: '404'
+      });
+      return false;
     }
+    // 发送请求
+    wx.request({
+      url: app._server + "/api/bx/get_repair_detail.php", 
+      data: {
+        "yktID": app._user.xs.ykth,
+        "bxID": _this.data.bxID
+      },
+      success: function(res) {
+        if(res.data.status === 200) {
+          var info = res.data.data;
+          //报修内容过滤标签
+          info.wx_bxnr = _this.convertHtmlToText(info.wx_bxnr);
+          //处理详情
+          var state = [{
+            'type': 'refused',
+            name: '驳回',
+            status: info.wx_wxztm == '驳回',
+            list: {}
+          },{
+            'type': 'finished',
+            name: '完工',
+            status: info.wx_wxztm == '已完工',
+            list: {
+              '用时': info.wx_ysfz + '分钟'
+            }
+          },{
+            'type': 'dispatched',
+            name: '派单',
+            status: !!info.wx_wxgm.trim(),
+            list: {
+              '承修人': info.wx_wxgm
+            }
+          },{
+            'type': 'accepted',
+            name: '受理',
+            status: !!info.wx_slr.trim(),
+            list: {
+              '受理人': info.wx_slr,
+              '承修部门': info.wx_cxbmm,
+              '响应时间': info.xysj
+            }
+          },{
+            'type': 'waited',
+            name: '审核',
+            status: !!info.wx_shr.trim(),
+            list: {
+              '审核人': info.wx_shr
+            }
+          },{
+            'type': 'waited',
+            name: '申请',
+            status: true,
+            list: {
+              '申请人': info.wx_bxr+' ('+info.wx_bxrrzm+')',
+              '申报时间': info.wx_bxsj
+            }
+          }];
+          _this.setData({
+            'detail': info,
+            'state': state.filter(function(e,i){
+              return e.status
+            }),
+            'remind': ''
+          });
+        }else{
+          app.showErrorModal(res.data.message);
+          _this.setData({
+            remind: res.data.message || '未知错误'
+          });
+        }
+      },
+      fail: function(res) {
+        app.showErrorModal(res.errMsg);
+        _this.setData({
+          remind: '网络错误'
+        });
+      },
+      complete: function(){
+        wx.stopPullDownRefresh();
+      }
+    });
   },
-  onLoad: function(){
-    console.log("页面启动");
+  convertHtmlToText: function(inputText){
+    var returnText = "" + inputText;
+    returnText = returnText.replace(/<\/?[^>]*>/g, '').replace(/[ | ]*\n/g, '\n').replace(/ /ig, '')
+                  .replace(/&mdash/gi,'-').replace(/&ldquo/gi,'“').replace(/&rdquo/gi,'”');
+    return returnText;
   },
-  onReady: function(){
-
-    this.repairDetail();
-  },
-  repairDetail: function ( ) {
-    var repairId = "1";
-
-    var repairDetailUrl = "/api/bx/get_repair_detail.php",
-        data = {
-          "yktID": app._user.xs.ykt_id,
-          "bxID": repairId
-        },
-        method = "GET";
-
-    console.log(repairDetailUrl, data, method);
-    sendRequest.apply(this, [ app, repairDetailUrl, data, method ]);
-  }
   
 });
