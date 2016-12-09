31f1a254f20fdb53971ff328916b81ed28661bb9
Cong Min <i@congm.in>
2016-12-09 13:46:46 +0800

    登录/完善页面动画

--- pages/more/append.js
+++ pages/more/append.js
@@ -34,9 +34,9 @@ Page({
       });
     }
     wx.onAccelerometerChange(function(res) {
-      var angle = -(res.x*24).toFixed(1);
-      if(angle>12){ angle=12; }
-      else if(angle<-12){ angle=-12; }
+      var angle = -(res.x*30).toFixed(1);
+      if(angle>14){ angle=14; }
+      else if(angle<-14){ angle=-14; }
       if(_this.data.angle !== angle){
         _this.setData({
           angle: angle
--- pages/more/login.js
+++ pages/more/login.js
@@ -19,9 +19,9 @@ Page({
       });
     }, 1000);
     wx.onAccelerometerChange(function(res) {
-      var angle = -(res.x*24).toFixed(1);
-      if(angle>12){ angle=12; }
-      else if(angle<-12){ angle=-12; }
+      var angle = -(res.x*30).toFixed(1);
+      if(angle>14){ angle=14; }
+      else if(angle<-14){ angle=-14; }
       if(_this.data.angle !== angle){
         _this.setData({
           angle: angle
